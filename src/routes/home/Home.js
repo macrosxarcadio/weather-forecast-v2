2d8ef5d33165e18e6bf7ef3f4d516eb9132a51f1
German Frate <frate_g@solarsistemas.net>
2023-02-22 15:34:31 -0300

    new feature chart for summarize information

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,8 +4,43 @@ import "./home.css";
 import { Link } from "react-router-dom";
 import sun from "../../assets/sun.png";
 import macross from "../../assets/macross.png";
-
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from '@faker-js/faker'
+import { useSelector } from "react-redux";
 const Home = () => {
+
+  const newWeather = useSelector((state) => state.forecast.value);
+
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      }
+    },
+    title: {
+      display: true,
+      text: 'Temp weather',
+    }
+  }
+
+/*   const labels = ['lunes', 'martes', 'jueves']; */
+
+  const data = {
+    labels:newWeather.map((wheather)=> wheather.dt_txt.split(' ').shift()),
+    datasets: [
+      {
+        label: 'dataset 1',
+        data: newWeather.map((wheather)=>wheather.main.temp),
+        borderColor: 'rgb(255,99,132,0.5)',
+        backgroundColor: 'rgb(53,162,235,0.5)'
+      }
+    ]
+  }
+
   return (
     <>
       <Col span={8}></Col>
@@ -51,6 +86,9 @@ const Home = () => {
           />
           MacrossArcadio
         </a>
+      </Col>
+      <Col span={24} >
+        <Line options={options} data={data} />
       </Col>
     </>
   );
